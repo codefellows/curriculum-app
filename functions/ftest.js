6b237c727ca0fd4a488df2fc417c25f67eb27e95
John Cokos <highlander.44@gmail.com>
2020-04-09 14:10:09 -0700

    added test fn

--- functions/ftest.js
+++ functions/ftest.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const github = require('./lib/github.js');
-
 exports.handler = async (event,context,callback) => {
 
+  const response = {
+    statusCode: 200,
+    body: ['hello','world'],
+  };
+
+  return response;
+
   try {
-
-    const repos = await github.getRepositories();
-
     return {
       statusCode: 200,
       body: repos,
