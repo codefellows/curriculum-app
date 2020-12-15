76c03336d01b578a2cfdc1b1927c1903798fb2e4
John Cokos <highlander.44@gmail.com>
2020-12-15 14:19:27 -0800

    testing live lambdas - does removing the log cause a RC?

--- functions/course.js
+++ functions/course.js
@@ -5,7 +5,6 @@ const coursesModel = require('./lib/courses.schema.js');
 exports.handler = async (event, context, callback) => {
 
   const id = event.pathParameters.id;
-  console.log(event, id);
   const courses = await coursesModel.query("id").eq(id).exec();
   const course = courses[0] || null;
 
