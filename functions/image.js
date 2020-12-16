460e01ce0d394046fcab0ef4f9f0aa65ac010d98
John Cokos <highlander.44@gmail.com>
2020-12-16 01:55:29 -0800

    image return

--- functions/image.js
+++ functions/image.js
@@ -8,15 +8,13 @@ exports.handler = async (event, context, callback) => {
 
   try {
     const { org, repo, version, path, image } = event.queryStringParameters;
-    console.log('qs', event.queryStringParameters);
     const source = `https://raw.githubusercontent.com/${org}/${repo}/${version}/${path}/${image}`;
-    console.log('source', source);
     const headers = { Authorization: `token ${process.env.TOKEN}` };
     let response = await axios.get(source, { responseType: 'arraybuffer', headers });
-    let imageSource = Buffer.from(response.data).toString('base64')
-    res.send(imageSource);
+    console.log('RESPONSE', response.data);
+    imageSource = Buffer.from(response.data).toString('base64')
   } catch (e) {
-    content.log(e.message);
+    console.log(e.message);
   }
 
   return {
