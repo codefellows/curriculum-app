f75203de80f3dd4290e7b765f5b2f7616e56b8b9
John Cokos <highlander.44@gmail.com>
2020-12-16 02:02:25 -0800

    image querysring format

--- functions/image.js
+++ functions/image.js
@@ -4,14 +4,13 @@ const axios = require('axios');
 
 exports.handler = async (event, context, callback) => {
 
-  const imageSource = 'R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
+  let imageSource = 'R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 
   try {
     const { org, repo, version, path, image } = event.queryStringParameters;
     const source = `https://raw.githubusercontent.com/${org}/${repo}/${version}/${path}/${image}`;
     const headers = { Authorization: `token ${process.env.TOKEN}` };
     let response = await axios.get(source, { responseType: 'arraybuffer', headers });
-    console.log('RESPONSE', response.data);
     imageSource = Buffer.from(response.data).toString('base64')
   } catch (e) {
     console.log(e.message);
